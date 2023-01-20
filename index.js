e3d01f3ebec1dabb50457d2acb9977fa33d63ca5
roselynelutomia <roselyne.lutomia@student.moringaschool.com>
2023-01-20 15:34:28 +0300

    My Completed Assignment

--- index.js
+++ index.js
@@ -1,19 +1,40 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
-}
+  for (let i = 0; i < array.length; i++) {
+    const numberAdd = target - array[i];
+    for (let j = i + 1; j < array.length; j++) {
+     (array[j] === numberAdd) 
+      return true;
+      }
+    }
+    return false;
+  }
 
 /* 
   Write the Big O time complexity of your function here
 */
-
+function hasTargetSum(array, target) {
+  for (let i = 0; i < array.length; i++) {
+    // n steps (depending on the length of the input array)
+    const numberAdd = target - array[i];
+    for (let j = i+1; j<array.length; j++) {
+       // n * n steps (nested loop!)
+     if(array[j] === numberAdd) 
+      return true;
+      }
+    }
+    //1 step
+    return false;
+  }
 /* 
   Add your pseudocode here
 */
-
+'get numbers that when added together equate the target number'
+'iterate for the remaining number'
+'check if the remaining number adds to target'
 /*
   Add written explanation of your solution here
 */
-
+'I need to get numbers that when added together equate the target number, iterate for the remaining number and check if the remaining number adds to target'
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
